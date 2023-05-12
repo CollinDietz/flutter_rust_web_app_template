1119ad057cb4341ebd281394d5ec3536ce13b549
Collin Dietz <collin.dietz@geappliances.com>
2023-05-11 23:21:56 -0400

    rustup configs

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,6 +1,10 @@
+rustup toolchain install nightly
+rustup +nightly component add rust-src
+rustup +nightly target add wasm32-unknown-unknown
+
+cargo install wasm-pack
+
 export RUSTUP_TOOLCHAIN=nightly
 export RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'
-
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 
 wasm-pack build -t no-modules -d `pwd`/web/pkg --no-typescript --out-name native --dev native -- -Z build-std=std,panic_abort
