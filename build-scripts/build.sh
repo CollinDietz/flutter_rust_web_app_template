14457bfde41e605bcb889cc61b570ade11d8cb57
Collin Dietz <collin.dietz@geappliances.com>
2023-05-11 23:08:47 -0400

    :man-shrug:

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,4 +1,6 @@
 export RUSTUP_TOOLCHAIN=nightly
 export RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'
 
+rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+
 wasm-pack build -t no-modules -d `pwd`/web/pkg --no-typescript --out-name native --dev native -- -Z build-std=std,panic_abort
