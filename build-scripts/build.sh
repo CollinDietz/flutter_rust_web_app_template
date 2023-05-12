3b89c07231dcd4143315283b65fbc7b9e60935d0
Collin Dietz <collin.dietz@geappliances.com>
2023-05-11 22:40:57 -0400

    Add wasm-pack dependency

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -2,4 +2,6 @@
 export RUSTUP_TOOLCHAIN=nightly
 export RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'
 
+cd native/ && cargo build
+
 wasm-pack build -t no-modules -d `pwd`/web/pkg --no-typescript --out-name native --dev native -- -Z build-std=std,panic_abort
